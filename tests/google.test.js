f67c6aef9d749cef83ce9688f57f1a4ee8c34c5f
Isaac Leite <isaacbonvivan@gmail.com>
2026-04-30 19:56:04 -0300

    fix: corrigindo teste google

--- tests/google.test.js
+++ tests/google.test.js
@@ -1,7 +1,16 @@
 const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome'); 
 
 async function testGoogle() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let options = new chrome.Options();
+  options.addArguments('--headless'); 
+  options.addArguments('--no-sandbox');
+  options.addArguments('--disable-dev-shm-usage');
+
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
 
   try {
     await driver.get('https://www.google.com');
@@ -11,9 +20,12 @@ async function testGoogle() {
     await searchBox.submit();
 
     await driver.wait(until.titleContains('Selenium'), 5000);
+    console.log("Teste de fumaça no Google: Sucesso!");
   } finally {
     await driver.quit();
   }
 };
 
-test('Google', async () => {await testGoogle()});
+test('Google Search Test', async () => {
+  await testGoogle();
+}, 15000); 
